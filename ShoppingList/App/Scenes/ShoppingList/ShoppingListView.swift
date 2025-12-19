49423643db95ebfd6a2b76100304a1c41aab9f69
Diggo Silva <rdiggosilva@gmail.com>
2025-12-19 09:12:09 -0300

    Created ShoppinListView, just imnplemented tabelView for now

--- ShoppingList/App/Scenes/ShoppingList/ShoppingListView.swift
+++ ShoppingList/App/Scenes/ShoppingList/ShoppingListView.swift
@@ -5,4 +5,40 @@
 //  Created by Diggo Silva on 19/12/25.
 //
 
-import Foundation
+import UIKit
+
+class ShoppingListView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubview(tableView)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
