72903775b2ea8428864079441160c0de3b4113d2
Diggo Silva <rdiggosilva@gmail.com>
2025-12-24 07:55:28 -0300

    Refactored custom cell

--- ShoppingList/App/Scenes/ShoppingList/Cells/ShoppingListCell.swift
+++ ShoppingList/App/Scenes/ShoppingList/Cells/ShoppingListCell.swift
@@ -18,9 +18,16 @@ final class ShoppingListCell: UITableViewCell {
     lazy var totalValueLabel = buildLabel(font: .systemFont(ofSize: 16, weight: .bold))
     lazy var quantityLabel = buildLabel(font: .systemFont(ofSize: 14, weight: .medium))
     lazy var quantityStepper = buildStepper(minValue: 1, maxValue: 50, target: self, action: #selector(quantitySelected))
-    lazy var vStack2 = buildStack(views: [totalValueLabel, quantityLabel, quantityStepper], spacing: 8, alignment: .trailing)
+    lazy var vStack2 = buildStack(views: [totalValueLabel, quantityLabel, quantityStepper], spacing: 4, alignment: .trailing)
     
     lazy var hStack = buildStack(views: [vStack1, vStack2], axis: .horizontal, spacing: 12, alignment: .center, distribution: .equalSpacing)
+    
+    lazy var barView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGreen
+        return view
+    }()
     
     var onQuantityChanged: ((Int) -> Void)?
     
@@ -43,14 +50,20 @@ final class ShoppingListCell: UITableViewCell {
     }
     
     private func setHierarchy() {
+        contentView.addSubview(barView)
         contentView.addSubview(hStack)
     }
     
     private func setConstraints() {
         let padding: CGFloat = 12
         NSLayoutConstraint.activate([
+            barView.topAnchor.constraint(equalTo: hStack.topAnchor, constant: padding),
+            barView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            barView.bottomAnchor.constraint(equalTo: hStack.bottomAnchor, constant: -padding),
+            barView.widthAnchor.constraint(equalToConstant: 1.5),
+            
             hStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
-            hStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            hStack.leadingAnchor.constraint(equalTo: barView.trailingAnchor, constant: 8),
             hStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
             hStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding)
         ])
