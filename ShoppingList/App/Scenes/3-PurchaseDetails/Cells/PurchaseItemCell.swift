ab4b476b4ea40e5c72138165c32a6ba4aaa43be6
Diggo Silva <rdiggosilva@gmail.com>
2025-12-27 09:29:23 -0300

    Created a summary view bellow tablView

---
+++ ShoppingList/App/Scenes/3-PurchaseDetails/Cells/PurchaseItemCell.swift
@@ -0,0 +1,8 @@
+//
+//  PurchaseItemCell.swift
+//  ShoppingList
+//
+//  Created by Diggo Silva on 26/12/25.
+//
+
+import Foundation
--- ShoppingList/App/Scenes/3-PurchaseDetails/PurchaseDetailsView.swift
+++ ShoppingList/App/Scenes/3-PurchaseDetails/PurchaseDetailsView.swift
@@ -12,9 +12,15 @@ final class PurchaseDetailsView: UIView {
     lazy var tableView: UITableView = {
         let tv = UITableView(frame: .zero, style: .insetGrouped)
         tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tv.register(PurchaseItemCell.self, forCellReuseIdentifier: PurchaseItemCell.identifier)
+        tv.allowsSelection = false
         return tv
     }()
+    
+    lazy var totalLabel = buildLabel(font: .preferredFont(forTextStyle: .headline))
+    lazy var totalItemsLabel = buildLabel(font: .preferredFont(forTextStyle: .subheadline))
+    lazy var totalUnitLabel = buildLabel(font: .preferredFont(forTextStyle: .subheadline))
+    lazy var vStack = buildStack(views: [totalLabel, totalItemsLabel, totalUnitLabel])
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,11 +32,18 @@ final class PurchaseDetailsView: UIView {
     private func setupView() {
         setHierarchy()
         setConstraints()
+        configStack()
     }
     
     private func setHierarchy() {
-        addSubviews(tableView)
+        addSubviews(tableView, vStack)
         backgroundColor = .systemBackground
+    }
+    
+    private func configStack() {
+        vStack.isLayoutMarginsRelativeArrangement = true
+        vStack.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        vStack.layer.cornerRadius = 8
     }
     
     private func setConstraints() {
@@ -38,7 +51,11 @@ final class PurchaseDetailsView: UIView {
             tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            tableView.bottomAnchor.constraint(equalTo: vStack.topAnchor),
+            
+            vStack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            vStack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            vStack.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
         ])
     }
 }
