000be929746ce57a372ee3ffc430a90c32d5ce8d
Diggo Silva <rdiggosilva@gmail.com>
2025-12-30 09:30:32 -0300

    Created tests for PurchaseDetailsViewModel

---
+++ ShoppingListTests/ViewModels/PurchaseDetailsViewModelTests.swift
@@ -0,0 +1,87 @@
+//
+//  PurchaseDetailsViewModelTests.swift
+//  ShoppingListTests
+//
+//  Created by Diggo Silva on 29/12/25.
+//
+
+import XCTest
+@testable import ShoppingList
+
+final class PurchaseDetailsViewModelTests: XCTestCase {
+    
+    private var sut: PurchaseDetailsViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        let items = [
+            MarketItem(name: "Leite", unitPrice: 5, quantity: 2),
+            MarketItem(name: "Pão", unitPrice: 2, quantity: 3)
+        ]
+        
+        let purchase = Purchase(date: Date(timeIntervalSince1970: 1000), items: items)
+        
+        sut = PurchaseDetailsViewModel(purchase: purchase)
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_totals_shouldReturnTheCorrectValues() {
+        XCTAssertEqual(sut.totalValue, 16)
+        XCTAssertEqual(sut.totalItems, 2)
+        XCTAssertEqual(sut.totalQuantity, 5)
+    }
+    
+    func test_numberOfRows_withoutFilters_shouldReturnTheCorrectValue() {
+        XCTAssertEqual(sut.numberOfRows(), 2)
+    }
+    
+    func test_itemForRow_withoutFilter_returnsCorrectItem() {
+        let item = sut.itemForRow(at: 0)
+        XCTAssertEqual(item.name, "Leite")
+    }
+    
+    func test_filterItem_byName_filtersCorrectly() {
+        sut.filterItems(with: "p")
+        XCTAssertEqual(sut.itemForRow(at: 0).name, "Pão")
+    }
+    
+    func test_filterItems_byQuantity_filtersCorrectly() {
+        sut.filterItems(with: "2")
+        XCTAssertEqual(sut.itemForRow(at: 0).name, "Leite")
+    }
+    
+    func test_filterItems_withEmptyText_resetsFilter() {
+        sut.filterItems(with: "lei")
+        sut.filterItems(with: "")
+
+        XCTAssertEqual(sut.numberOfRows(), 2)
+        XCTAssertEqual(sut.currentSearchText(), "")
+    }
+    
+    func test_resetFilter_restoresOriginalItems() {
+        sut.filterItems(with: "lei")
+        sut.resetFilter()
+        
+        XCTAssertEqual(sut.numberOfRows(), 2)
+    }
+    
+    func test_currentSearchText_returnsLastSearch() {
+        sut.filterItems(with: "pao")
+        XCTAssertEqual(sut.currentSearchText(), "pao")
+    }
+    
+    func test_exportText_containsPurchaseSummary() {
+        let text = sut.exportText()
+        
+        XCTAssertTrue(text.contains("DETALHES DA COMPRA"))
+        XCTAssertTrue(text.contains("Leite"))
+        XCTAssertTrue(text.contains("Pão"))
+        XCTAssertTrue(text.contains("Itens"))
+        XCTAssertTrue(text.contains("Unidades"))
+        XCTAssertTrue(text.contains("TOTAL"))
+    }
+}
